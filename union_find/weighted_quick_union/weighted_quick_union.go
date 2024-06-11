051318b72187d35f1b75afadd12e8ee4bbdc9231
Carlos Barragan <carlosbarraganr@gmail.com>
2024-06-11 12:53:27 -0600

    feat(union-find): Weighted quick union algorithm added

---
+++ union_find/weighted_quick_union/weighted_quick_union.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+/*
+ * Weighted Quick Union
+ * Complexity: Olog
+ */
+func main() {
+	array := make([]int, 10)
+	size := make([]int, 10)
+
+	for i := 0; i < 10; i++ {
+		array[i] = i
+	}
+
+	fmt.Println(array) // [0 1 2 3 4 5 6 7 8 9]
+
+	union(4, 3, &array, &size)
+	union(3, 8, &array, &size)
+	union(6, 5, &array, &size)
+	union(9, 4, &array, &size)
+	union(2, 1, &array, &size)
+	union(5, 0, &array, &size)
+	union(7, 2, &array, &size)
+	union(6, 1, &array, &size)
+	union(6, 1, &array, &size)
+	union(7, 3, &array, &size)
+
+	fmt.Println(connected(4, 9, &array)) // true
+	fmt.Println(array)                   // [6 2 6 4 6 6 6 2 4 4]
+}
+
+func root(p int, array *[]int) int {
+	if (*array)[p] != p {
+		return root((*array)[p], array)
+	}
+	return (*array)[p]
+}
+
+func union(p int, q int, array *[]int, size *[]int) {
+	var i = root(p, array)
+	var j = root(q, array)
+
+	// if same root they are already united
+	if i == j {
+		return
+	}
+
+	// we weight wich tree is smaller
+	if (*size)[i] < (*size)[j] {
+		(*array)[i] = j
+		// add initial weight on first union j + i
+		if (*size)[j] == 0 {
+			(*size)[j] = 2
+		}
+		(*size)[j] += (*size)[i]
+	} else {
+		(*array)[j] = i
+		// add initial weight on first union i + j
+		if (*size)[i] == 0 {
+			(*size)[i] = 2
+		}
+		(*size)[i] += (*size)[j]
+	}
+}
+
+func connected(p int, q int, array *[]int) bool {
+	return root(p, array) == root(q, array)
+}
