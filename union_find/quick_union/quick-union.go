9645e20058eba45d8162c5b62b6b03dc8c49f09e
Carlos Barragan <carlosbarraganr@gmail.com>
2024-06-11 12:02:42 -0600

    feat(union-find): Quick union lazy algorithm added

---
+++ union_find/quick_union/quick-union.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*
+ * Quick Union (lazy)
+ * Complexity: ON
+ */
+func main() {
+	array := make([]int, 10)
+
+	for i := 0; i < 10; i++ {
+		array[i] = i
+	}
+
+	fmt.Println(array) // [0 1 2 3 4 5 6 7 8 9]
+
+	union(3, 8, &array)
+	union(4, 3, &array)
+	union(6, 5, &array)
+
+	fmt.Println(connected(4, 9, &array)) // false
+	fmt.Println(connected(3, 8, &array)) // true
+	fmt.Println(connected(4, 8, &array)) // true
+
+	fmt.Println(array) // [0 1 2 8 3 6 6 7 8 9]
+}
+
+func root(p int, array *[]int) int {
+	if (*array)[p] != p {
+		return root((*array)[p], array)
+	}
+	return (*array)[p]
+}
+
+func union(p int, q int, array *[]int) {
+	(*array)[root(p, array)] = q
+}
+
+func connected(p int, q int, array *[]int) bool {
+	return root(p, array) == root(q, array)
+}
