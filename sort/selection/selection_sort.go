958dbd8c1b40ece806cbc9bc2e947454a8c6b577
CarlosBarraganR <carlosbarraganr@gmail.com>
2024-06-14 18:04:55 -0600

    feat(sort): selection sort added

--- sort/selection/selection_sort.go
+++ sort/selection/selection_sort.go
@@ -5,32 +5,27 @@ import (
 	"time"
 )
 
-type Array struct {
-	items []int
-}
-
-func (array *Array) sort() {
-	for i := range array.items {
-		for j := i + 1; j < len(array.items); j++ {
-			if array.items[j] < array.items[i] {
-				array.swap(i, j)
+func Sort[T int | float32 | string](array []T) {
+	for i := range array {
+		for j := i + 1; j < len(array); j++ {
+			if array[j] < array[i] {
+				array[i], array[j] = array[j], array[i]
 			}
 		}
 	}
 }
 
-func (array *Array) swap(a int, b int) {
-	oldValue := array.items[a]
-	array.items[a] = array.items[b]
-	array.items[b] = oldValue
-}
-
 func main() {
 	start := time.Now()
-	array := &Array{[]int{14, 6, 232, 5, 63, 2, 1, 5}}
-	fmt.Println(array) // [14 6 232 5 63 2 1 5]
-	array.sort()
-	fmt.Println(array) // [1 2 5 5 6 14 63 232]
+	arrayInt := []int{14, 6, 232, 5, 63, 2, 1, 5}
+	fmt.Println(arrayInt) // [14 6 232 5 63 2 1 5]
+	Sort(arrayInt)
+	fmt.Println(arrayInt) // [1 2 5 5 6 14 63 232]
+
+	arrayString := []string{"z", "b", "f", "a", "y", "e", "r", "p"}
+	fmt.Println(arrayString) // [z b f a y e r p]
+	Sort(arrayString)
+	fmt.Println(arrayString) // [a b e f p r y z]
 
 	elapsed := time.Since(start) // Calculate the elapsed time
 	fmt.Printf("Execution time: %s\n", elapsed)
