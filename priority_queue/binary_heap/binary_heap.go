6995047ad3ae5c3b4f191e513544a091eebffe95
CarlosBarraganR <carlosbarraganr@gmail.com>
2024-06-27 21:01:25 -0600

    feat(priotity-queue): PQ refator param name from bp to bh

--- priority_queue/binary_heap/binary_heap.go
+++ priority_queue/binary_heap/binary_heap.go
@@ -9,58 +9,58 @@ type BinaryHeap[T int | float32 | string] struct {
 	items []T
 }
 
-func (pq *BinaryHeap[T]) swim(index int) {
+func (bh *BinaryHeap[T]) swim(index int) {
 	// calculate parent from tree
 	parent := (index / 2) - 1
 	// swim up until the top or minor than parent
 	for parent > 0 {
 		// if parent is minor than current index, swap index for parent
-		if pq.items[parent] < pq.items[index] {
-			pq.items[parent], pq.items[index] = pq.items[index], pq.items[parent]
+		if bh.items[parent] < bh.items[index] {
+			bh.items[parent], bh.items[index] = bh.items[index], bh.items[parent]
 		}
 		index = parent            // move up to parent position
 		parent = (parent / 2) - 1 // find next parent index
 	}
 }
 
-func (pq *BinaryHeap[T]) sink(index int) {
-	n := len(pq.items)
+func (bh *BinaryHeap[T]) sink(index int) {
+	n := len(bh.items)
 	// Swim down while the element has at least one child
 	for (2*index + 1) < n {
 		j := 2*index + 1 // left child index
 
 		// if right child exists and is greater than left, move to right child index
-		if j+1 < n && pq.items[j] < pq.items[j+1] {
+		if j+1 < n && bh.items[j] < bh.items[j+1] {
 			j++
 		}
 		// if the current element is greater or equal than largest child, stop the sinking because index is in place
-		if pq.items[index] >= pq.items[j] {
+		if bh.items[index] >= bh.items[j] {
 			break
 		}
 		// Swap the current element with the largest child
-		pq.items[index], pq.items[j] = pq.items[j], pq.items[index]
+		bh.items[index], bh.items[j] = bh.items[j], bh.items[index]
 		index = j // move down to the swaped child position
 	}
 }
 
-func (pq *BinaryHeap[T]) IsEmpty() bool {
-	return len(pq.items) == 0
+func (bh *BinaryHeap[T]) IsEmpty() bool {
+	return len(bh.items) == 0
 }
 
-func (pq *BinaryHeap[T]) Push(value T) {
-	pq.items = append(pq.items, value)
-	pq.swim(len(pq.items) - 1)
+func (bh *BinaryHeap[T]) Push(value T) {
+	bh.items = append(bh.items, value)
+	bh.swim(len(bh.items) - 1)
 }
 
-func (pq *BinaryHeap[T]) Pop() T {
+func (bh *BinaryHeap[T]) Pop() T {
 	// store next in queue
-	valueToPop := pq.items[0]
+	valueToPop := bh.items[0]
 	// exchange root [0] for last [n-1]
-	pq.items[0], pq.items[len(pq.items)-1] = pq.items[len(pq.items)-1], pq.items[0]
+	bh.items[0], bh.items[len(bh.items)-1] = bh.items[len(bh.items)-1], bh.items[0]
 	// slice to remove last item
-	pq.items = pq.items[:len(pq.items)-1]
+	bh.items = bh.items[:len(bh.items)-1]
 	// sink switched root until correct position
-	pq.sink(0)
+	bh.sink(0)
 
 	return valueToPop
 }
