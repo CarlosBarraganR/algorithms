28bc46a702d788acd48a589542cff544e5e28aed
CarlosBarraganR <carlosbarraganr@gmail.com>
2024-07-04 14:25:50 -0600

    feat(binary-tree): Binary Tree struct pointer fix and Size method

--- binary_tree/binary_tree.go
+++ binary_tree/binary_tree.go
@@ -12,15 +12,16 @@ type TreeNode struct {
 	value TreeNodeValue
 	left  *TreeNode
 	right *TreeNode
+	count int
 }
 
 type BinaryTree struct {
-	root TreeNode
+	root *TreeNode
 }
 
 func (bt *BinaryTree) Search(key string) TreeNodeValue {
 	// always start on root of the tree
-	currentNode := &bt.root
+	currentNode := bt.root
 	// iterate until value is found or no more TreeNodes are available
 	for currentNode != nil {
 		if key < currentNode.key { // less moves to the left
@@ -34,24 +35,38 @@ func (bt *BinaryTree) Search(key string) TreeNodeValue {
 	return nil
 }
 
+func (bt *BinaryTree) _count(node *TreeNode) int {
+	// fmt.Println("node", node)
+	if node == nil {
+		return 0
+	}
+	return node.count
+}
+
+func (bt *BinaryTree) Size() int {
+	return bt._count(bt.root)
+}
+
 func (bt *BinaryTree) _put(node *TreeNode, key string, value TreeNodeValue) *TreeNode {
 	// recursive base condition to stop at the bottom of the tree
 	if node == nil {
-		return &TreeNode{key: key, value: value}
-	}
-	if key < node.key {
+		return &TreeNode{key: key, value: value, count: 1}
+	} else if key < node.key {
 		node.left = bt._put(node.left, key, value)
 	} else if key > node.key {
 		node.right = bt._put(node.right, key, value)
 	} else {
 		node.value = value // overrides old value
 	}
+
+	node.count = 1 + bt._count(node.left) + bt._count(node.right)
+
 	return node
 }
 
 func (bt *BinaryTree) Insert(key string, value TreeNodeValue) {
 	// recursive way to insert and reset links all the way up
-	_ = bt._put(&bt.root, key, value)
+	bt.root = bt._put(bt.root, key, value)
 }
 
 func (bt *BinaryTree) _floor(node *TreeNode, key string) *TreeNode {
@@ -72,29 +87,33 @@ func (bt *BinaryTree) _floor(node *TreeNode, key string) *TreeNode {
 }
 
 func (bt *BinaryTree) Floor(key string) *TreeNode {
-	// recursive way to search and find key or closest value
-	return bt._floor(&bt.root, key)
+	// recursive way to search and find key or closest (round down) value
+	return bt._floor(bt.root, key)
 }
 
 func main() {
 	start := time.Now()
 
-	binaryTree := &BinaryTree{TreeNode{key: "s", value: "S",
-		left: &TreeNode{key: "e", value: "E",
-			left: &TreeNode{key: "a", value: "A",
-				right: &TreeNode{key: "c", value: "C"}},
-			right: &TreeNode{key: "r", value: "R",
-				left: &TreeNode{key: "h", value: "H",
-					right: &TreeNode{key: "m", value: "M"}}}},
-		right: &TreeNode{key: "x", value: "X"}}}
+	binaryTree := &BinaryTree{}
+
+	binaryTree.Insert("s", "S")
+	binaryTree.Insert("e", "E")
+	binaryTree.Insert("a", "A")
+	binaryTree.Insert("r", "R")
+	binaryTree.Insert("c", "C")
+	binaryTree.Insert("h", "H")
+	binaryTree.Insert("m", "M")
+	binaryTree.Insert("x", "X")
 
 	fmt.Println(binaryTree.Search("h")) // H
 	fmt.Println(binaryTree.Search("g")) // null
-	fmt.Println(binaryTree.Floor("h"))  // E
+	fmt.Println(binaryTree.Floor("g"))  // E
+	fmt.Println(binaryTree.Size())      // 8
 	binaryTree.Insert("g", "G")
 	fmt.Println(binaryTree.Search("g")) // G
 	binaryTree.Insert("g", "new G")
 	fmt.Println(binaryTree.Search("g")) // new G
+	fmt.Println(binaryTree.Size())      // 9
 
 	elapsed := time.Since(start) // Calculate the elapsed time
 	fmt.Printf("Execution time: %s\n", elapsed)
